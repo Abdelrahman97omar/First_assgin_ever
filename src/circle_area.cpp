d27a4b3d01d00da98b048519f2a42b66ca80a992
Abdelrahman97omar <abdelrahman19972022@gmail.com>
2023-03-31 03:49:07 +0200

    find circle area

---
+++ src/circle_area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x;
+    const float pi =3.14;
+    cout <<"enter circle's raduis" << endl;
+    cin >> x;
+    cout <<"area is "<< x*x*pi << endl;
+    return 0;
+}
