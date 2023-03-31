52cd0bb5b5df23fdafca2affe6a1c4a6d1366ba3
Abdelrahman97omar <abdelrahman19972022@gmail.com>
2023-03-31 03:25:40 +0200

    calculator

---
+++ bulid/CMakeFiles/first-cmake-try.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first-cmake-try.dir/src/first.cpp.o"
+  "first-cmake-try"
+  "first-cmake-try.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/first-cmake-try.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
